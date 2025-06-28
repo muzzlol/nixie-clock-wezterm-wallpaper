8d76feb036a51046938fa0d58394fac8f9d927d7
muzzlol <muzxmmilkhxn@gmail.com>
2025-06-28 08:10:52 +0530

    ah

--- wezterm.lua
+++ wezterm.lua
@@ -15,11 +15,14 @@ config.keys = {
     { key = 'DownArrow', mods = 'CMD|SHIFT', action = wezterm.action.ActivatePaneDirection("Down") },
     { key = '5', mods = 'CMD|SHIFT|ALT|CTRL', action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
     { key = ';', mods = 'CMD|SHIFT|ALT|CTRL', action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
-}
-
--- < Nixie Clock code
+  }
+  
+  -- < Nixie Clock code
+  
+  -- Clock updates every second.
+  config.status_update_interval = 1000
+  
 local function get_time_parts()
-  if wezterm.strftime then
     local time_str = wezterm.strftime('%H%M%S')
     return {
       time_str:sub(1, 1),
@@ -29,16 +32,6 @@ local function get_time_parts()
       time_str:sub(5, 5),
       time_str:sub(6, 6),
     }
-  end
-  local now = os.date('*t')
-  return {
-    string.format('%02d', now.hour):sub(1, 1),
-    string.format('%02d', now.hour):sub(2, 2),
-    string.format('%02d', now.min):sub(1, 1),
-    string.format('%02d', now.min):sub(2, 2),
-    string.format('%02d', now.sec):sub(1, 1),
-    string.format('%02d', now.sec):sub(2, 2),
-  }
 end
 
 local image_width = 110
@@ -96,51 +89,67 @@ local function init_nixie_layers()
   end
 
   wezterm.GLOBAL.nixie_layers = layers
-  wezterm.GLOBAL.last_time_parts = { '', '', '', '', '', '' }
+  wezterm.GLOBAL.last_time_parts = { 'x', 'x', 'x', 'x', 'x', 'x' } -- Force update on first run
 end
 
 init_nixie_layers()
 
+local time_part_to_layer_idx = {
+  [1] = 2, -- H1
+  [2] = 3, -- H2
+  [3] = 5, -- M1
+  [4] = 6, -- M2
+  [5] = 8, -- s1
+  [6] = 9, -- s2
+}
+
 wezterm.on('update-status', function(window, pane)
   init_nixie_layers()
 
-  local time_parts = get_time_parts()
-
-  wezterm.GLOBAL.separator_state = (wezterm.GLOBAL.separator_state + 1) % 2
-  local sep_idx = wezterm.GLOBAL.separator_state + 1 -- 1 or 2
-
-  local desired_sources = {
-    digit_sources[time_parts[1]],
-    digit_sources[time_parts[2]],
-    separator_sources[sep_idx],
-    digit_sources[time_parts[3]],
-    digit_sources[time_parts[4]],
-    separator_sources[sep_idx],
-    digit_sources[time_parts[5]],
-    digit_sources[time_parts[6]],
-  }
-
+  local current_time_parts = get_time_parts()
+  local last_time_parts = wezterm.GLOBAL.last_time_parts
   local layers = wezterm.GLOBAL.nixie_layers
   local changed = false
 
-  for i, src in ipairs(desired_sources) do
-    local layer = layers[i + 1]
-
-    if layer.source ~= src then
-      layer.source = src
-      changed = true
+  -- Loop over the time parts, from seconds to hours, as seconds are
+  -- most likely to have changed.
+  for i = 6, 1, -1 do
+    if current_time_parts[i] ~= last_time_parts[i] then
+      local layer_idx = time_part_to_layer_idx[i]
+      local new_digit_source = digit_sources[current_time_parts[i]]
+      if layers[layer_idx].source ~= new_digit_source then
+        layers[layer_idx].source = new_digit_source
+        changed = true
+      end
     end
   end
 
+  -- Update the blinking separators, which change every second
+  wezterm.GLOBAL.separator_state = (wezterm.GLOBAL.separator_state + 1) % 2
+  local sep_idx = wezterm.GLOBAL.separator_state + 1 -- will be 1 or 2
+  local new_separator_source = separator_sources[sep_idx]
+
+  -- Separator between HH and MM is at layer index 4
+  if layers[4].source ~= new_separator_source then
+    layers[4].source = new_separator_source
+    changed = true
+  end
+  -- Separator between MM and SS is at layer index 7
+  if layers[7].source ~= new_separator_source then
+    layers[7].source = new_separator_source
+    changed = true
+  end
+
   if changed then
+    -- Store the new time for the next comparison
+    wezterm.GLOBAL.last_time_parts = current_time_parts
+    -- Apply the configuration changes
     local overrides = window:get_config_overrides() or {}
     overrides.background = layers
     window:set_config_overrides(overrides)
   end
 end)
 
--- Clock updates every second.
-config.status_update_interval = 1000
 
 -- Nixie clock >
 
